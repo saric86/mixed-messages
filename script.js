130e7d623327e8bf609308b2c98f2c88496537c4
emir <emir_work@hotmail.com>
2024-06-01 17:35:16 +0200

    first commit random inspirational quotes

---
+++ script.js
@@ -0,0 +1,32 @@
+const words = {
+    needs: ["thinker", "planner", "leader", "motivator", "lover"],
+    feel: ["inspiring", "encouraging", "enraged", "amusing", "like the world is not enough"],
+    should: ["try something new", "run at least for 30 minutes", "talk to a stranger", "learn something new", "chill and drink beer"]
+}
+
+// empty quotes that will be filled later in a loop
+let todaysQuote = []
+
+// generates a random number so that it can be used to select a string from a objects array
+function randomnumber (num) {
+    return Math.floor(Math.random() * num)
+}
+
+// loops through the object properties and randomly selects one string from the arrray
+for (const object in words) {
+
+    let number = randomnumber(words[object].length);
+    //console.log(number);
+
+        if (object == 'needs') {
+            todaysQuote.push(`>> Everyone needs a ${words[object][number]}`);
+        } else if (object == 'feel') {
+            todaysQuote.push(`>> Today, you feel ${words[object][number]}`);
+        } else if (object == 'should') {
+            todaysQuote.push(`>> Today you should ${words[object][number]}`);
+        }
+}
+
+// formats the three quotes so the output is readable 
+let formatedQuotes = todaysQuote.join('\n')
+console.log(formatedQuotes);
